9a2c69c8c6847601f80195fe8edc681ef91ae6bd
Gio Mendoza <mendoza.gionino@gmail.com>
2014-12-12 22:14:56 +0800

    test

--- LANCOMMS/src/lancomms/LANCOMMS.java
+++ LANCOMMS/src/lancomms/LANCOMMS.java
@@ -6,21 +6,22 @@
 package lancomms;
 
 import javax.swing.JFrame;
-
+import com.sun.security.auth.module.NTSystem;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
-import com.github.sarxos.webcam.WebcamResolution;
 
 public class LANCOMMS {
+
     public static void main(String[] args) throws InterruptedException {
-        
+        NTSystem NTSystem = new NTSystem();//test
+        System.out.println(NTSystem.getName());//test
         Webcam webcam = Webcam.getDefault();
         WebcamPanel panel = new WebcamPanel(webcam);
-        
+
         panel.setFPSDisplayed(true);
         panel.setDisplayDebugInfo(true);
         panel.setImageSizeDisplayed(true);
-        panel.setMirrored(true);
+//        panel.setMirrored(true);
 
         JFrame window = new JFrame("Test webcam panel");
         window.add(panel);
@@ -30,4 +31,7 @@ public class LANCOMMS {
         window.setVisible(true);
     }
 
+    public static void authTest(String filename) {
+
+    }
 }
